531b7ad8a966fc9a074f4930af130908be2f771b
ljadhavstc <ljadhav.c@stc.com.bh>
2025-12-17 21:24:11 +0530

    lint issues fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,21 @@ module.exports = {
     {
       files: ['*.ts', '*.tsx'],
       rules: {
-        '@typescript-eslint/no-shadow': ['error'],
+        '@typescript-eslint/no-shadow': ['warn'],
+        '@typescript-eslint/no-unused-vars': ['warn'],
         'no-shadow': 'off',
         'no-undef': 'off',
+        'no-unused-vars': 'warn',
+        'react-hooks/exhaustive-deps': 'warn',
+        'react-native/no-inline-styles': 'warn',
+        'no-useless-escape': 'warn',
+      },
+    },
+    {
+      files: ['*.js'],
+      rules: {
+        'no-unused-vars': 'warn',
+        'no-shadow': 'warn',
       },
     },
   ],
